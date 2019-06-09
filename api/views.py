218f194b62981225d3ff75064b658e11ca81c0c0
Teddy Coder <teddy_coder@mail.ru>
2019-06-09 18:06:30 +0700

    Сделано получение списка объявлений через REST API

---
+++ api/views.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from main.models import Bb
+from .serializers import BbSerializer
+
+@api_view(['GET'])
+def bbs(request):
+    if request.method == 'GET':
+        bbs = Bb.objects.filter(is_active=True)[:10]
+        serializer = BbSerializer(bbs, many=True)
+        return Response(serializer.data)
--- bboard/settings.py
+++ bboard/settings.py
@@ -42,11 +42,15 @@ INSTALLED_APPS = [
     'django_cleanup',
     'easy_thumbnails',
     'captcha',
+    'rest_framework',
+    'corsheaders',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -145,3 +149,6 @@ THUMBNAIL_ALIASES = {
     },
 }
 THUMBNAIL_BASEDIR = 'thumbnails'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
