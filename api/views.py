5a817e8c8cf103b49ef9b6d1a6c438a1b4d41f7e
Teddy Coder <teddy_coder@mail.ru>
2019-06-09 18:19:18 +0700

    Добавлено просмотр объявления через REST API

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import RetrieveAPIView
 
 from main.models import Bb
-from .serializers import BbSerializer
+from .serializers import BbSerializer, BbDetailSerializer
+
 
 @api_view(['GET'])
 def bbs(request):
@@ -10,3 +12,8 @@ def bbs(request):
         bbs = Bb.objects.filter(is_active=True)[:10]
         serializer = BbSerializer(bbs, many=True)
         return Response(serializer.data)
+
+
+class BbDetailView(RetrieveAPIView):
+        queryset = Bb.objects.filter(is_active=True)
+        serializer_class = BbDetailSerializer
